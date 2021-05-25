60111626e748a3e508adb92cfcd1b01d83828cfa
Angelo Reis <angelo.desenvolvedor@gmail.com>
2021-05-25 20:19:29 -0300

    Feat/add styled component on main component (#4)
    * feat: novas versões do components do boilerplate
    
    * feat: finalizando correção do jest no boilerplate
    
    * feat: implementando storybook
    
    * feat: add logo to component main
    
    * feat: add global style
    
    * refactor: adjustment on files - storybook
    
    Co-authored-by: Angelo Reis <angelo.reis@kumulus.com.br>

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,21 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-    <h2>Já gostei de PHP</h2>
-  </main>
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Advanced',
+  description = 'Typescript, ReactJS, NextJS and Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="image of Atom and written right: React Advanced course"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="A developer guy in front a computer"
+    />
+  </S.Wrapper>
 )
 
 export default Main
