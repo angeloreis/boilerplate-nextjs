1d5d4d2f8397711f1123ff18df426e5f8e7b03c8
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-10-18 18:14:53 -0300

    fix: add new addons to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,20 @@
 module.exports = {
   staticDirs: ['../public'],
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials', 'storybook-addon-next-router'],
+  addons: [
+    '@storybook/addon-essentials',
+    'storybook-addon-next-router',
+    '@storybook/addon-links',
+    '@storybook/addon-controls',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+        sourceLoaderOptions: null,
+        transcludeMarkdown: true
+      }
+    }
+  ],
   core: {
     builder: 'webpack5'
   },
