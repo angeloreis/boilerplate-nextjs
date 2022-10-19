9aeb983f8dfdf36cf18f9859f0013052fcd7343f
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-10-18 21:58:53 -0300

    fix: update props of interface Button

--- src/types/atomic-design.d.ts
+++ src/types/atomic-design.d.ts
@@ -1,6 +1,7 @@
 export interface IButton {
   classType: 'primary' | 'success' | 'info' | 'alert' | 'danger' | 'disable'
-  width?: string
   textOfButton: string
+  width?: string
+  isLoading?: boolean
   onClick?: () => void
 }
